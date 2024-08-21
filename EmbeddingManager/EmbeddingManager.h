5b3675694250602d3198b45d24fe7c6d933b99d1
Abhiram Chennupati <achennupati03@berkeley.edu>
2024-08-21 01:37:33 -0700

    Initial Commit

---
+++ EmbeddingManager/EmbeddingManager.h
@@ -0,0 +1,12 @@
+//
+//  EmbeddingManager.h
+//  EmbeddingManager
+//
+//  Created by Abhiram Chennupati on 8/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmbeddingManager : NSObject
+
+@end
---
+++ EmbeddingManager/EmbeddingManager.m
@@ -0,0 +1,12 @@
+//
+//  EmbeddingManager.m
+//  EmbeddingManager
+//
+//  Created by Abhiram Chennupati on 8/21/24.
+//
+
+#import "EmbeddingManager.h"
+
+@implementation EmbeddingManager
+
+@end
