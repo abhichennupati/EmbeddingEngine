4b424b1f9016e59b43ca3dbb529a13ca5ef997ab
Abhiram Chennupati <achennupati03@berkeley.edu>
2024-08-21 22:22:24 -0700

    added empty GetEmbedding function

---
+++ EmbeddingManager/BertHelper.swift
@@ -0,0 +1,12 @@
+//
+//  BertHelper.swift
+//  EmbeddingManager
+//
+//  Created by Abhiram Chennupati on 8/21/24.
+//
+
+import Foundation
+
+func GetEmbedding(Tokens: [Int]) -> [Float] {
+    return []
+}
